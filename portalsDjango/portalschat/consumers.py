42c0c8998ccbded20d9b284c4f0486036cbd7e3b
lasan <limahaliyana@gmail.com>
2024-02-26 11:16:19 +0530

    added basic chatroom functionality backend

--- portalsDjango/portalschat/consumers.py
+++ portalsDjango/portalschat/consumers.py
@@ -1,6 +1,8 @@
 import json
 
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from .models import SimpleUser,ChatRoom
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -14,24 +16,45 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         # Leave room group
+        await self.channel_layer.group_send(
+            self.room_group_name, {"type": 'chat.leave', "username": self.username}
+        )
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+        #db cleanup
+        #check if user in other rooms. if not, delete user
+        #check if room has no users. if so, delete room
+        user = await database_sync_to_async(SimpleUser.objects.get)(username=self.username)
+        chatroom = await database_sync_to_async(ChatRoom.objects.get)(name=self.room_name)
+        await database_sync_to_async(chatroom.users.remove)(user.id)
+        if await database_sync_to_async(chatroom.users.count)() == 0:
+            await database_sync_to_async(chatroom.delete)()
+        if await database_sync_to_async(user.chatrooms.count)() == 0:
+            await database_sync_to_async(user.delete)()
+     
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {**{"type": 'chat_message'}, **text_data_json}
+            self.room_group_name, {**{"type": 'chat.message'}, **text_data_json}
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
+        await self.send(text_data=json.dumps(event))
+
+    async def chat_leave(self,event):
+        await self.send(text_data=json.dumps(event))
+
+    async def chat_initial(self, event):
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
-
-    async def chat_newUserJoined(self, event):
-
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"username": event["username"],'message': "sent by newuserjoined"}))
+        await self.send(text_data=json.dumps({"type": "chat.initial", "username": event["username"]}))
+        self.username = event["username"]
+        # database stuff    
+        user,_ = await database_sync_to_async(SimpleUser.objects.get_or_create)(username=event["username"])
+        chatroom,__ = await database_sync_to_async(ChatRoom.objects.get_or_create)(name=self.room_name)
+        await database_sync_to_async(chatroom.users.add)(user.id)
--- portalsDjango/portalschat/models.py
+++ portalsDjango/portalschat/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class SimpleUser(models.Model):
+    username = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.username
+
+class ChatRoom(models.Model):
+    name = models.CharField(max_length=200)
+    users = models.ManyToManyField(SimpleUser, related_name='chatrooms')
+
+    def __str__(self):
+        return self.name
+
+    def users_online(self):
+        return self.users.count()
